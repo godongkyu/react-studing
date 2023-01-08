5c5b9bd982f7a266ae9be5dc035b723be645ac01
godongkyu <103103792+godongkyu@users.noreply.github.com>
2023-01-08 20:38:00 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import './App.css';
+import { useState } from 'react';
+import Header from './component/Header';
+import Nav from './component/Nav';
+import Article from './component/Article';
+import Create from './component/Create';
+import Update from './component/Update';
+
+function App() {
+  // const _mode = useState('WELCOME');
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+  // console.log('_mode', mode);
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+
+  const [topics, setTopics] = useState([
+    {id: 1, title: 'html', body: 'html is...'},
+    {id: 2, title: 'css', body: 'css is...'},
+    {id: 3, title: 'js', body: 'js is...'},
+  ])
+
+  let content = null;
+  let contextControl = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"/>
+  }else if(mode === 'READ'){
+    let title, body = null;
+    for(let i = 0; i < topics.length; i++) {
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}/>
+    contextControl= <>
+    <li><a href={'/update'+id} onClick={(event)=>{
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>
+    <li><input type="button" value="Delete" onClick={()=>{
+      const newTopics = [];
+      for(let i=0; i<topics.length; i++) {
+        if(topics[i].id !== id ) {
+          newTopics.push(topics[i]);
+        }
+      }
+      setTopics(newTopics);
+      setMode('WELCOME');
+    }}/></li>
+    </>
+  }else if (mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id: nextId, title: _title, body: _body};
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+        }}/>;
+  } else if (mode === 'UPDATE') {
+    let title, body = null;
+    for(let i = 0; i < topics.length; i++) {
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body) =>{
+      console.log(title, body);
+      const newTopics = [...topics]
+      const updatedTopic = {id:id, title: title, body: body};
+      for(let i=0; i<newTopics.length; i++) {
+        if(newTopics[i].id === id) {
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>
+  }
+  return (
+    <div>
+      <Header title="WEB" onChangeMode={()=>{
+        setMode('WELCOME');
+      }}/>
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
+      }}/>
+      {content}
+      <ul>
+      <li><a href="/create" onClick={(event)=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
+      </li>
+      {contextControl}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
